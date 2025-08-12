1e9129d17d9d9a508e5e4c36ef5b5190e44c3bb9
signor1 <emmanuelomemgboji@gmail.com>
2025-08-12 04:10:47 +0100

    feat: init commit with file/folder structure

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{App, HttpServer, middleware::Logger};
+
+mod routes;
+mod utils;
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        unsafe {
+            std::env::set_var("RUST_LOG", "actix_web=info");
+        };
+    }
+
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    let port = (*utils::constants::PORT).clone();
+    let address = (*utils::constants::ADDRESS).clone();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .configure(routes::home_routes::config)
+    })
+    .bind((address, port))?
+    .run()
+    .await
+}
